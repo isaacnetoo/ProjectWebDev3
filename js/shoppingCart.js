6b29bdbaffa184febe845acc21755f08ede4c9d6
George <killyen444@gmail.com>
2024-03-05 16:31:10 +0800

    Initial commit

---
+++ js/shoppingCart.js
@@ -0,0 +1,21 @@
+class OurPetCartClass {
+    constructor(pid, pname,category, price, amount, rating){
+
+        this.pid = pid ; 
+        this.name = pname; 
+        this.category = category;
+        this.rating=Number(rating);
+        this.price = Number(price); 
+        this.amount = Number(amount);
+
+    }
+
+
+
+    cartTotal() {
+        return (this.price * this.amount).toFixed(2);
+    }
+}
+
+
+export default  OurPetCartClass;   // need to export to allow the outer page access
